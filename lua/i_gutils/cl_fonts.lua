725fef1d74cdead70ece2e985bcc1d3b14e83420
Pdzly <34943569+Pdzly@users.noreply.github.com>
2022-10-31 15:56:02 +0000

    added getTextSize function

--- lua/i_gutils/cl_fonts.lua
+++ lua/i_gutils/cl_fonts.lua
@@ -7,7 +7,9 @@ local function makeFont(args, key)
         size = args[1],
         weight = args[2],
     })
+
     fonts[key] = key
+
     return key
 end
 
@@ -19,10 +21,28 @@ end
 ]]
 function getFont(...)
     local args = {...}
+
     args[1] = args[1] or 18
     args[2] = args[2] or 500
     args[3] = args[3] or "Roboto"
-
     local key = table.concat(args, ",")
+
     return fonts[key] or makeFont(args, key)
+end
+
+-- returns size of the text with the given font ( or already set font )
+--[[
+    1 = text
+    2 = font
+]]
+function getTextSize(...)
+    local args = {...}
+
+    args[1] = args[1] or "ABC"
+
+    if args[2] then
+        surface.SetFont(args[2])
+    end
+
+    return {surface.GetTextSize(args[1])}
 end
